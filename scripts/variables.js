8913c3214eba61649b085caa694667c4aeb00b2c
ausmull22 <ahm85@drexel.edu>
2022-02-10 14:56:39 -0500

    Added Array of Zodiac Signs
    Also added additional comments

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,7 +1,15 @@
-let birthdate;
-let birthmonth;
-let day; 
-let sign;
+let date; // User's birthdate 
+let birthmonth;  // User's birth month 
+let day;  // User's day of the month 
+let sign; // Array of Zodiacs
+let selected_sign // Selected zodiac sign 
+
+
+// Array of zodiac sign 
+const sign_type =  ['aquarius', 'pisces', 'aries', 'taurus', 'gemini', 'leo', 
+'virgo', 'libra', 'scorpio', 'sagittarius', 'capricorn']
+
+// Grab date input:
 
 function getdate() {
     let date = document.getElementById("input").value;
