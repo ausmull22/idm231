22a4249af3c3f76d77058cb6551e913fb24f066d
ausmull22 <ahm85@drexel.edu>
2022-02-10 21:23:59 -0500

    Began writing Popup Screen (HTML/CSS)
    When you enter the page, a popup screen will appear giving you instructions on how to use the program.

--- scripts/variables.js
+++ scripts/variables.js
@@ -3,6 +3,7 @@ let birthmonth;  // User's birth month
 let day;  // User's day of the month 
 let sign; // Array of Zodiacs
 let selected_sign // Selected zodiac sign 
+let image
 
 
 // Array of zodiac sign 
@@ -22,6 +23,36 @@ function getdate() {
     console.log(birthmonth)
     console.log(day)
 
+}
+
+let image_view = 'zodiac';
+
+
+if (image_view == 'cereal') {
+
+    let image = document.getElementById('aries');
+    image.src = "images/symbols/zodiac/aries.png";
 
 }
+
+if (image_view == 'zodiac') {
+    
+
+    let image = document.getElementById('aries');
+    image.src = "images/symbols/brand/luckycharms.png";
+
+}
+
+function switchbutton() {
+
+    if (image_view == 'zodiac') {
+        let image_view = 'cereal'; //If the Zodiac signs are on screen, switch them to icons of the cereals.
+    }
+
+    if (image_view == 'cereal') {
+        let image_view = 'zodiac'; //If the cereal signs are on screen, switch them to icons of the zodiacs.
+    }
+
+}
+
 
