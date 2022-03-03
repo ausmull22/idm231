b0b1d1915f6e0b0f58353e4acaaea167de383547
ausmull22 <ahm85@drexel.edu>
2022-03-02 22:17:23 -0500

    Adding function image_switch()
    This will allow the buttons on the bottom left to switch from being zodiacs to cereal brands.

--- scripts/variables.js
+++ scripts/variables.js
@@ -56,3 +56,64 @@ function zodiac_exit() {
 
 }
 
+
+// Change Navigation Images
+
+
+// Image Switch to cereal
+
+image_tracker ='f';
+
+function image_switch() {
+    if (image_tracker=='f') {
+        document.getElementById("aries").src = "images/symbols/brand/aries.png"; 
+        document.getElementById("taurus").src = "images/symbols/brand/taurus.png"; 
+        document.getElementById("gemini").src = "images/symbols/brand/gemini.png"; 
+        document.getElementById("cancer").src = "images/symbols/brand/cancer.png"; 
+        document.getElementById("leo").src = "images/symbols/brand/leo.png"; 
+        document.getElementById("virgo").src = "images/symbols/brand/virgo.png"; 
+        document.getElementById("libra").src = "images/symbols/brand/libra.png"; 
+        document.getElementById("scorpio").src = "images/symbols/brand/scorpio.png"; 
+        document.getElementById("sagittarius").src = "images/symbols/brand/sagittarius.png"; 
+        document.getElementById("capricorn").src = "images/symbols/brand/capricorn.png"; 
+        document.getElementById("aquarius").src = "images/symbols/brand/aquarius.png"; 
+        document.getElementById("pisces").src = "images/symbols/brand/pisces.png"; 
+        image_tracker = 't';
+    } else {
+        document.getElementById("aries").src = "images/symbols/brand/luckycharms.png"; 
+        document.getElementById("taurus").src = "images/symbols/brand/trix.png"; 
+        document.getElementById("gemini").src = "images/symbols/brand/raisinbran.png"; 
+        document.getElementById("cancer").src = "images/symbols/brand/frostedflake.png"; 
+        document.getElementById("leo").src = "images/symbols/brand/life.png"; 
+        document.getElementById("virgo").src = "images/symbols/brand/alphabits.png"; 
+        document.getElementById("libra").src = "images/symbols/brand/applejacks.png"; 
+        document.getElementById("scorpio").src = "images/symbols/brand/cornflakes.png"; 
+        document.getElementById("sagittarius").src = "images/symbols/brand/honeynutcheerios.png"; 
+        document.getElementById("capricorn").src = "images/symbols/brand/goldengrahams.png"; 
+        document.getElementById("aquarius").src = "images/symbols/brand/wheaties.png"; 
+        document.getElementById("pisces").src = "images/symbols/brand/reeses.png"; 
+        image_tracker = 'f';
+    }
+}
+
+
+
+// Zodiac Switches
+
+
+
+function zodiac_saggitarus() { // Switch to Saggitarus
+
+    document.getElementById("js-cereal-title").innerHTML = "Honey Nut Cheerios";
+    document.getElementById("js-zodiac-title").innerHTML = "Saggitarus";
+    document.getElementById("js-written-description").innerHTML = "ADD TEXT HERE";
+    
+}
+
+function zodiac_saggitarus() { // Switch to Saggitarus
+
+    document.getElementById("js-cereal-title").innerHTML = "Honey Nut Cheerios";
+    document.getElementById("js-zodiac-title").innerHTML = "Saggitarus";
+    document.getElementById("js-written-description").innerHTML = "ADD TEXT HERE";
+    
+}
